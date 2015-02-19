c5cddb7fbaadfbf4246bb6e6d3174e7c67960ce8
James Newnorth <trikisatan@gmail.com>
2015-02-19 12:32:15 +0100

    Added templating to Framework\Newnorth\EMail

--- Framework/Newnorth/EMail.php
+++ Framework/Newnorth/EMail.php
@@ -2,34 +2,68 @@
 namespace Framework\Newnorth;
 
 class EMail {
+	/* Variables */
+
 	private $From = null;
+
 	private $ReplyTo = null;
+
 	private $Subject = null;
+
 	private $Text = null;
+
 	private $Html = null;
 
 	/* Methods */
+
 	public function SetFrom($From) {
 		$this->From = $From;
 	}
+
 	public function SetReplyTo($ReplyTo) {
 		$this->ReplyTo = $ReplyTo;
 	}
+
 	public function SetSubject($Subject) {
 		$this->Subject = $Subject;
 	}
+
 	public function SetText($Text) {
 		$this->Text = $Text;
 	}
+
+	public function SetTextTemplate($Template, $Variables = []) {
+		$FilePath = $Template.'.text.'.Application::GetLocale().'.phtml';
+
+		$this->Text = file_get_contents($FilePath);
+
+		foreach($Variables as $Key => $Value) {
+			$this->Text = str_replace('%'.$Key.'%', $Value, $this->Text);
+		}
+	}
+
 	public function SetHtml($Html) {
 		$this->Html = $Html;
 	}
+
+	public function SetHtmlTemplate($Template, $Variables = []) {
+		$FilePath = $Template.'.html.'.Application::GetLocale().'.phtml';
+
+		$this->Html = file_get_contents($FilePath);
+
+		foreach($Variables as $Key => $Value) {
+			$this->Html = str_replace('%'.$Key.'%', $Value, $this->Html);
+		}
+	}
+
 	public function Send($To) {
 		$Rel = 'PHP-rel-'.md5(rand());
 		$Alt = 'PHP-alt-'.md5(rand());
 		$Newline = "\r\n";
 
-		$Headers = 'Content-Type: multipart/related; boundary="'.$Rel.'"';
+		$Headers =
+			'Content-Type: multipart/related; boundary="'.$Rel.'"'.$Newline.
+			'Date: '.date('r');
 
 		if($this->From !== null) {
 			$Headers .= "\r\n".'From: '.$this->From;
