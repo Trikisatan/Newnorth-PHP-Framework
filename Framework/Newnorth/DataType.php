f8feaca27ff8bd5a5e2a6c28fcb34d07f8bc8572
Roselyne <roselyne.lavalley@gmail.com>
2015-04-01 16:11:56 +0200

    Added "Array" to public function __construct

--- Framework/Newnorth/DataType.php
+++ Framework/Newnorth/DataType.php
@@ -4,6 +4,6 @@ namespace Framework\Newnorth;
 abstract class DataType {
 	/* Magic methods */
 
-	public abstract function __construct($Data);
+	public abstract function __construct(Array $Data);
 }
 ?>
