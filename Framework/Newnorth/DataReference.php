2662ca1ff066f272cb0f601daae6cf8721d5b9a2
James Newnorth <trikisatan@gmail.com>
2015-09-21 11:57:06 +0200

    Added support for multiple keys in for data reference

--- Framework/Newnorth/DataReference.php
+++ Framework/Newnorth/DataReference.php
@@ -4,13 +4,15 @@ namespace Framework\Newnorth;
 class DataReference {
 	/* Instance variables */
 
-	public $LocalKey;
+	public $KeyCount;
+
+	public $LocalKeys;
 
 	public $ForeignDataManager;
 
-	public $ForeignKey;
-
 	public $ForeignPrimaryKey;
+
+	public $ForeignKeys;
 
 	public $Alias;
 
@@ -19,13 +21,70 @@ class DataReference {
 	/* Magic methods */
 
 	public function __construct($Parameters) {
-		$this->LocalKey = $Parameters['LocalKey'];
+		if(!isset($Parameters['LocalKeys'])) {
+			throw new RuntimeException(
+				'DataReference requires the parameter "LocalKeys".',
+				[]
+			);
+		}
+		else if(!is_array($Parameters['LocalKeys'])) {
+			throw new RuntimeException(
+				'DataReference requires the parameter "LocalKeys" to be an array.',
+				[]
+			);
+		}
+		else if(count($Parameters['LocalKeys']) === 0) {
+			throw new RuntimeException(
+				'DataReference requires the parameter "LocalKeys" to not be an empty array.',
+				[]
+			);
+		}
+		else if(!isset($Parameters['ForeignDataManager'])) {
+			throw new RuntimeException(
+				'DataReference requires the parameter "ForeignDataManager".',
+				[]
+			);
+		}
+		else if(!isset($Parameters['ForeignKeys'])) {
+			throw new RuntimeException(
+				'DataReference requires the parameter "ForeignKeys".',
+				[]
+			);
+		}
+		else if(!is_array($Parameters['ForeignKeys'])) {
+			throw new RuntimeException(
+				'DataReference requires the parameter "ForeignKeys" to be an array.',
+				[]
+			);
+		}
+		else if(count($Parameters['ForeignKeys']) === 0) {
+			throw new RuntimeException(
+				'DataReference requires the parameter "ForeignKeys" to not be an empty array.',
+				[]
+			);
+		}
+		else if(!isset($Parameters['Alias'])) {
+			throw new RuntimeException(
+				'DataReference requires the parameter "Alias".',
+				[]
+			);
+		}
+		else if(count($Parameters['LocalKeys']) !== count($Parameters['ForeignKeys'])) {
+			throw new RuntimeException(
+				'DataReference requires the parameters "LocalKeys" and "ForeignKeys" to be equal sized arrays.',
+				[]
+			);
+		}
+
+		$this->KeyCount = count($Parameters['LocalKeys']);
+
+		$this->LocalKeys = $Parameters['LocalKeys'];
 
 		$this->ForeignDataManager = $Parameters['ForeignDataManager'];
 
 		$this->ForeignPrimaryKey = $Parameters['ForeignDataManager']->PrimaryKey;
 
-		$this->ForeignKey = $Parameters['ForeignKey'];
+		$this->ForeignKeys = $Parameters['ForeignKeys'];
 
 		$this->Alias = $Parameters['Alias'];
 
@@ -44,7 +103,20 @@ class DataReference {
 
 	public function Load(\Framework\Newnorth\DataType $DataType) {
 		if(!$DataType->{'Is'.$this->Alias.'Loaded'}) {
-			$DataType->{$this->Alias} = $this->ForeignDataManager->{'FindBy'.$this->ForeignKey->Name}($DataType->{$this->LocalKey->Name});
+			$Query = $this->ForeignDataManager->CreateSelectQuery();
+
+			$Query->Conditions = new \Framework\Newnorth\DbAnd();
+
+			for($I = 0; $I < $this->KeyCount; ++$I) {
+				if($this->LocalKeys[$I] instanceof \Framework\Newnorth\ADataMember) {
+					$Query->Conditions->EqualTo($this->ForeignKeys[$I], $DataType->{$this->LocalKeys[$I]->Name});
+				}
+				else {
+					$Query->Conditions->EqualTo($this->ForeignKeys[$I], $this->LocalKeys[$I]);
+				}
+			}
+
+			$DataType->{$this->Alias} = $this->ForeignDataManager->FindByQuery($Query);
 
 			$DataType->{'Is'.$this->Alias.'Loaded'} = true;
 		}
@@ -63,7 +135,10 @@ class DataReference {
 	public function Remove(\Framework\Newnorth\DataType $DataType) {
 		$this->Load($DataType);
 
-		$DataType->{$this->Alias}->{'Set'.$this->ForeignKey->Name}(null);
+		foreach($this->ForeignKeys as $ForeignKey)
+		{
+			$DataType->{$this->Alias}->{'Set'.$ForeignKey->Name}(null);
+		}
 
 		$DataType->{$this->Alias} = null;
 	}
