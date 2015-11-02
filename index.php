ad30f3f342217fc5b88ac4f7a06488b8bfa21c3c
James Newnorth <trikisatan@gmail.com>
2015-11-02 08:51:57 +0100

    Added functions to get application, layout and page.

--- index.php
+++ index.php
@@ -9,8 +9,20 @@ require('Framework/MySQL/MySQL.php');
 
 /* Framework methods */
 
+function A() {
+	return $GLOBALS['Application'];
+}
+
 function DM($Alias) {
 	return $GLOBALS['Application']->GetDataManager($Alias);
+}
+
+function L() {
+	return $GLOBALS['Layout'];
+}
+
+function P() {
+	return $GLOBALS['Page'];
 }
 
 /* Execution of application */
